62cda00e7d2a3d02fce88464ff582e9e2e201f4c
Nomi <nomiwen@outlook.de>
2023-07-16 21:49:36 +0200

    Add serial data reads, Read Status and ID commands

--- src/core/nand.cpp
+++ src/core/nand.cpp
@@ -18,17 +18,31 @@ constexpr u64 PAGE_SIZE_ECC = PAGE_SIZE + 16;
 constexpr u64 BLOCK_SIZE = 32 * PAGE_SIZE_ECC;
 constexpr u64 NAND_SIZE  = 2048 * BLOCK_SIZE;
 
+constexpr u32 NAND_ID = 0x35EC;
+
 enum class NANDReg {
     STATUS  = 0x1D101004,
     COMMAND = 0x1D101008,
+    PAGE  = 0x1D10100C,
     RESET = 0x1D101014,
     DMAPAGE = 0x1D101020,
     DMACTRL = 0x1D101024,
     DMASTATUS = 0x1D101028,
+    DMAINTR = 0x1D101038,
+    RESUME  = 0x1D101200,
+    SERIALDATA = 0x1D101300,
 };
 
 enum class NANDCommand {
+    READ_STATUS = 0x70,
+    READ_ID = 0x90,
     RESET = 0xFF,
+};
+
+enum class NANDStatus {
+    ERASE_ERROR  = 1 << 0,
+    DEVICE_READY = 1 << 6,
+    WRITE_PROTECT = 1 << 7,
 };
 
 enum STATUS {
@@ -45,10 +59,42 @@ enum DMACTRL {
 std::array<u8, NAND_SIZE> nand;
 std::array<u8, PAGE_SIZE_ECC> nandBuffer;
 
-u32 status, dmapage, dmactrl;
+// NAND serial data
+std::array<u8, BLOCK_SIZE> serialData;
+u32 serialIdx, serialSize;
+
+u32 status, nandpage, dmapage, dmactrl, dmaintr;
+
+u8 deviceStatus; // NAND chip status
+
+void setSerialSize(u32 size) {
+    serialSize = size;
+    serialIdx  = 0;
+}
+
+u32 getSerialData() {
+    const auto data = serialData[serialIdx];
+
+    serialIdx = (serialIdx + 1) % serialSize;
+
+    return data;
+}
 
 void doCommand(u8 cmd) {
     switch ((NANDCommand)cmd) {
+        case NANDCommand::READ_STATUS:
+            std::puts("[NAND    ] Read Status");
+
+            serialData[0] = deviceStatus;
+            setSerialSize(1);
+            break;
+        case NANDCommand::READ_ID:
+            std::puts("[NAND    ] Read ID");
+
+            serialData[0] = (u8)NAND_ID;
+            serialData[1] = (u8)(NAND_ID >> 8);
+            setSerialSize(2);
+            break;
         case NANDCommand::RESET:
             std::puts("[NAND    ] Reset");
 
@@ -82,10 +128,14 @@ void init(const char *nandPath) {
     std::printf("[NAND    ] Loading NAND image \"%s\"\n", nandPath);
     assert(loadFile(nandPath, nand.data(), NAND_SIZE));
 
+    deviceStatus = (u32)NANDStatus::DEVICE_READY;
+
     std::puts("[NAND    ] OK");
 }
 
 u32 read(u32 addr) {
+    u32 data;
+
     switch ((NANDReg)addr) {
         case NANDReg::STATUS:
             std::puts("[NAND    ] Read @ STATUS");
@@ -96,19 +146,36 @@ u32 read(u32 addr) {
         case NANDReg::DMASTATUS:
             std::puts("[NAND    ] Read @ DMASTATUS");
             return 0;
+        case NANDReg::DMAINTR:
+            std::puts("[NAND    ] Read @ DMAINTR");
+            return dmaintr;
+        case NANDReg::SERIALDATA:
+            std::puts("[NAND    ] Read @ SERIALDATA");
+            
+            return getSerialData();
         default:
             std::printf("[NAND    ] Unhandled read @ 0x%08X\n", addr);
 
             exit(0);
     }
+
+    return data;
 }
 
 void write(u32 addr, u32 data) {
     switch ((NANDReg)addr) {
+        case NANDReg::STATUS: // ??
+            std::printf("[NAND    ] Write @ STATUS = 0x%08X\n", data);
+            break;
         case NANDReg::COMMAND:
             std::printf("[NAND    ] Write @ COMMAND = 0x%08X\n", data);
 
             doCommand(data);
+            break;
+        case NANDReg::PAGE:
+            std::printf("[NAND    ] Write @ PAGE = 0x%08X\n", data);
+
+            nandpage = (data >> 10) & 0x1FFFF;
             break;
         case NANDReg::RESET:
             std::printf("[NAND    ] Write @ RESET = 0x%08X\n", data);
@@ -124,6 +191,14 @@ void write(u32 addr, u32 data) {
             dmactrl = data;
 
             if (data & DMA_BUSY) doDMA(data & TO_NAND, data & PAGE_DATA_EN, data & SPARE_DATA_EN);
+            break;
+        case NANDReg::DMAINTR:
+            std::printf("[NAND    ] Write @ DMAINTR = 0x%08X\n", data);
+
+            dmaintr = data;
+            break;
+        case NANDReg::RESUME:
+            std::printf("[NAND    ] Write @ RESUME = 0x%08X\n", data);
             break;
         default:
             std::printf("[NAND    ] Unhandled write @ 0x%08X = 0x%08X\n", addr, data);
