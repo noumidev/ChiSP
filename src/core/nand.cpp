845fa75cecdf69e812a49cdded7bc0913bffbf86
Nomi <nomiwen@outlook.de>
2023-08-23 21:49:55 +0200

    Make NAND timings more accurate

--- src/core/nand.cpp
+++ src/core/nand.cpp
@@ -15,7 +15,7 @@
 
 namespace psp::nand {
 
-constexpr i64 NAND_OP_CYCLES = 1 << 16;
+constexpr i64 NAND_OP_CYCLES = 60 * scheduler::_1MS;
 
 constexpr u64 PAGE_SIZE = 512;
 constexpr u64 PAGE_SIZE_ECC = PAGE_SIZE + 16;
