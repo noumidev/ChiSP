f9cc5db7335e903e70431319d375b4257dd91a28
Nomi <nomiwen@outlook.de>
2023-08-24 10:39:23 +0200

    Stub unknown register

--- src/core/ddr.cpp
+++ src/core/ddr.cpp
@@ -10,42 +10,47 @@
 
 namespace psp::ddr {
 
-u32 unknown[8];
+u32 unknown[9];
 
 enum class DDRReg {
+    UNKNOWN0 = 0x1D000000,
     FLUSH = 0x1D000004,
-    UNKNOWN0 = 0x1D000020,
-    UNKNOWN1 = 0x1D000024,
-    UNKNOWN2 = 0x1D00002C,
-    UNKNOWN3 = 0x1D000030,
-    UNKNOWN4 = 0x1D000034,
-    UNKNOWN5 = 0x1D000038,
-    UNKNOWN6 = 0x1D000040,
-    UNKNOWN7 = 0x1D000044,
+    UNKNOWN1 = 0x1D000020,
+    UNKNOWN2 = 0x1D000024,
+    UNKNOWN3 = 0x1D00002C,
+    UNKNOWN4 = 0x1D000030,
+    UNKNOWN5 = 0x1D000034,
+    UNKNOWN6 = 0x1D000038,
+    UNKNOWN7 = 0x1D000040,
+    UNKNOWN8 = 0x1D000044,
 };
 
 u32 read(u32 addr) {
     switch ((DDRReg)addr) {
-        case DDRReg::FLUSH:
-            std::puts("[DDR     ] Read @ FLUSH");
-
-            return 0;
         case DDRReg::UNKNOWN0:
             std::printf("[DDR     ] Unknown read @ 0x%08X\n", addr);
 
             return unknown[0];
-        case DDRReg::UNKNOWN2:
+        case DDRReg::FLUSH:
+            std::puts("[DDR     ] Read @ FLUSH");
+
+            return 0;
+        case DDRReg::UNKNOWN1:
             std::printf("[DDR     ] Unknown read @ 0x%08X\n", addr);
 
-            return unknown[2];
+            return unknown[1];
         case DDRReg::UNKNOWN3:
             std::printf("[DDR     ] Unknown read @ 0x%08X\n", addr);
 
             return unknown[3];
-        case DDRReg::UNKNOWN6:
+        case DDRReg::UNKNOWN4:
             std::printf("[DDR     ] Unknown read @ 0x%08X\n", addr);
 
-            return unknown[6];
+            return unknown[4];
+        case DDRReg::UNKNOWN7:
+            std::printf("[DDR     ] Unknown read @ 0x%08X\n", addr);
+
+            return unknown[7];
         default:
             std::printf("[DDR     ] Unhandled read @ 0x%08X\n", addr);
 
@@ -55,13 +60,13 @@ u32 read(u32 addr) {
 
 void write(u32 addr, u32 data) {
     switch ((DDRReg)addr) {
-        case DDRReg::FLUSH:
-            std::printf("[DDR     ] Write @ FLUSH = 0x%08X\n", data);
-            break;
         case DDRReg::UNKNOWN0:
             std::printf("[DDR     ] Unknown write @ 0x%08X = 0x%08X\n", addr, data);
 
             unknown[0] = data;
+            break;
+        case DDRReg::FLUSH:
+            std::printf("[DDR     ] Write @ FLUSH = 0x%08X\n", data);
             break;
         case DDRReg::UNKNOWN1:
             std::printf("[DDR     ] Unknown write @ 0x%08X = 0x%08X\n", addr, data);
@@ -97,6 +102,11 @@ void write(u32 addr, u32 data) {
             std::printf("[DDR     ] Unknown write @ 0x%08X = 0x%08X\n", addr, data);
 
             unknown[7] = data;
+            break;
+        case DDRReg::UNKNOWN8:
+            std::printf("[DDR     ] Unknown write @ 0x%08X = 0x%08X\n", addr, data);
+
+            unknown[8] = data;
             break;
         default:
             std::printf("[DDR     ] Unhandled write @ 0x%08X = 0x%08X\n", addr, data);
