5f95f4d1daabd20317b0ad7edab72da94a184bfd
Nomi <nomiwen@outlook.de>
2024-01-29 17:26:26 +0100

    Add read/write handlers

--- include/sys/memory.hpp
+++ include/sys/memory.hpp
@@ -5,16 +5,35 @@
 
 #pragma once
 
+#include <type_traits>
+
 #include "common/types.hpp"
 
 namespace sys::memory {
+
+constexpr u32 PAGE_SHIFT = 12;
+constexpr u32 PAGE_SIZE = 1 << PAGE_SHIFT;
+constexpr u32 PAGE_MASK = PAGE_SIZE - 1;
 
 // Memory region base addresses
 namespace MemoryBase {
     enum : u32 {
         BootROM = 0x1FC00000,
+    };
+}
+
+// Memory region sizes
+namespace MemorySize {
+    enum : u32 {
+        BootROM = 0x1000,
         AddressSpace = 0x20000000,
     };
+}
+
+constexpr int PAGE_NUM = MemorySize::AddressSpace >> PAGE_SHIFT;
+
+inline u32 maskAddress(const u32 vaddr) {
+    return vaddr & (MemorySize::AddressSpace - 1);
 }
 
 void init(const char *bootPath);
@@ -22,12 +41,15 @@ void deinit();
 
 void reset();
 
+// Returns a pointer to raw memory (unsafe!)
+void *getPointer(u32 vaddr);
+
 // Reads data from system memory
 template<typename T>
-T read(u32 vaddr);
+T read(u32 vaddr) requires std::is_unsigned_v<T>;
 
 // Writes data to system memory
 template<typename T>
-void write(u32 vaddr, T data);
+void write(u32 vaddr, const T data) requires std::is_unsigned_v<T>;
 
 }
--- src/sys/memory.cpp
+++ src/sys/memory.cpp
@@ -5,14 +5,94 @@
 
 #include "sys/memory.hpp"
 
+#include <array>
+#include <cstdlib>
+#include <cstring>
+#include <ios>
+
 #include <plog/Log.h>
 
 #include "common/file.hpp"
 
 namespace sys::memory {
 
+// Memory arrays
+std::array<u8, MemorySize::BootROM> bootROM;
+
+// Page table
+std::array<u8 *, PAGE_NUM> pageTable;
+
 void init(const char *bootPath) {
-    (void)bootPath;
+    reset();
+
+    PLOG_INFO << "Loading boot ROM \"" << bootPath << "\"";
+
+    readFile(bootPath, bootROM.data(), MemorySize::BootROM);
+}
+
+void deinit() {}
+
+void reset() {
+    // Clear page table
+    for (u32 i = 0; i < PAGE_NUM; i++) {
+        pageTable[i] = NULL;
+    }
+}
+
+void *getPointer(u32 vaddr) {
+    // Mask virtual address
+    vaddr = maskAddress(vaddr);
+
+    const u32 page = vaddr >> PAGE_SHIFT;
+
+    if (pageTable[page] != NULL) {
+        return (void *)&pageTable[page][vaddr & PAGE_MASK];
+    }
+
+    PLOG_FATAL << "Invalid pointer (address = " << std::hex << vaddr << ")";
+
+    exit(0);
+}
+
+template<typename T>
+T read(u32 vaddr) {
+    // Mask virtual address
+    vaddr = maskAddress(vaddr);
+
+    const u32 page = vaddr >> PAGE_SHIFT;
+
+    // Look up address in page table, return data directly if it's mapped
+    if (pageTable[page] != NULL) {
+        T data;
+        std::memcpy(&data, &pageTable[page][vaddr & PAGE_MASK], sizeof(T));
+
+        return data;
+    } else {
+        switch (vaddr) {
+            PLOG_FATAL << "Unrecognized read" << 8 * sizeof(T) << " (addr = " << std::hex << vaddr << ")";
+
+            exit(0);
+        }
+    }
+}
+
+template<typename T>
+void write(u32 vaddr, const T data) {
+    // Mask virtual address
+    vaddr = maskAddress(vaddr);
+
+    const u32 page = vaddr >> PAGE_SHIFT;
+
+    // Look up address in page table, write data directly if it's mapped
+    if (pageTable[page] != NULL) {
+        std::memcpy(&pageTable[page][vaddr & PAGE_MASK], &data, sizeof(T));
+    } else {
+        switch (vaddr) {
+            PLOG_FATAL << "Unrecognized write" << 8 * sizeof(T) << " (addr = " << std::hex << vaddr << ", data" << data << ")";
+
+            exit(0);
+        }
+    }
 }
 
 }
