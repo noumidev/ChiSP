fd6780eec36094bbb662ba8878365bd743186b87
Nomi <nomiwen@outlook.de>
2024-01-29 20:20:55 +0100

    Instantiate read and write

--- src/sys/memory.cpp
+++ src/sys/memory.cpp
@@ -33,10 +33,10 @@ void init(const char *bootPath) {
 void deinit() {}
 
 void reset() {
+    PLOG_INFO << "Memory reset";
+
     // Clear page table
-    for (u32 i = 0; i < PAGE_NUM; i++) {
-        pageTable[i] = NULL;
-    }
+    pageTable.fill(NULL);
 }
 
 void *getPointer(u32 vaddr) {
@@ -54,8 +54,8 @@ void *getPointer(u32 vaddr) {
     exit(0);
 }
 
-template<typename T>
-T read(u32 vaddr) {
+template<>
+u8 read(u32 vaddr) {
     // Mask virtual address
     vaddr = maskAddress(vaddr);
 
@@ -63,21 +63,65 @@ T read(u32 vaddr) {
 
     // Look up address in page table, return data directly if it's mapped
     if (pageTable[page] != NULL) {
-        T data;
-        std::memcpy(&data, &pageTable[page][vaddr & PAGE_MASK], sizeof(T));
-
-        return data;
+        return pageTable[page][vaddr & PAGE_MASK];
     } else {
         switch (vaddr) {
-            PLOG_FATAL << "Unrecognized read" << 8 * sizeof(T) << " (addr = " << std::hex << vaddr << ")";
+            default:
+                PLOG_FATAL << "Unrecognized read8 (addr = " << std::hex << vaddr << ")";
 
-            exit(0);
+                exit(0);
         }
     }
 }
 
-template<typename T>
-void write(u32 vaddr, const T data) {
+template<>
+u16 read(u32 vaddr) {
+    // Mask virtual address
+    vaddr = maskAddress(vaddr);
+
+    const u32 page = vaddr >> PAGE_SHIFT;
+
+    // Look up address in page table, return data directly if it's mapped
+    if (pageTable[page] != NULL) {
+        u16 data;
+        std::memcpy(&data, &pageTable[page][vaddr & PAGE_MASK], sizeof(u16));
+
+        return data;
+    } else {
+        switch (vaddr) {
+            default:
+                PLOG_FATAL << "Unrecognized read16 (addr = " << std::hex << vaddr << ")";
+
+                exit(0);
+        }
+    }
+}
+
+template<>
+u32 read(u32 vaddr) {
+    // Mask virtual address
+    vaddr = maskAddress(vaddr);
+
+    const u32 page = vaddr >> PAGE_SHIFT;
+
+    // Look up address in page table, return data directly if it's mapped
+    if (pageTable[page] != NULL) {
+        u32 data;
+        std::memcpy(&data, &pageTable[page][vaddr & PAGE_MASK], sizeof(u32));
+
+        return data;
+    } else {
+        switch (vaddr) {
+            default:
+                PLOG_FATAL << "Unrecognized read32 (addr = " << std::hex << vaddr << ")";
+
+                exit(0);
+        }
+    }
+}
+
+template<>
+void write(u32 vaddr, const u8 data) {
     // Mask virtual address
     vaddr = maskAddress(vaddr);
 
@@ -85,12 +129,53 @@ void write(u32 vaddr, const T data) {
 
     // Look up address in page table, write data directly if it's mapped
     if (pageTable[page] != NULL) {
-        std::memcpy(&pageTable[page][vaddr & PAGE_MASK], &data, sizeof(T));
+        pageTable[page][vaddr & PAGE_MASK] = data;
     } else {
         switch (vaddr) {
-            PLOG_FATAL << "Unrecognized write" << 8 * sizeof(T) << " (addr = " << std::hex << vaddr << ", data" << data << ")";
+            default:
+                PLOG_FATAL << "Unrecognized write8 (addr = " << std::hex << vaddr << ", data = " << (u32)data << ")";
 
-            exit(0);
+                exit(0);
+        }
+    }
+}
+
+template<>
+void write(u32 vaddr, const u16 data) {
+    // Mask virtual address
+    vaddr = maskAddress(vaddr);
+
+    const u32 page = vaddr >> PAGE_SHIFT;
+
+    // Look up address in page table, write data directly if it's mapped
+    if (pageTable[page] != NULL) {
+        std::memcpy(&pageTable[page][vaddr & PAGE_MASK], &data, sizeof(u16));
+    } else {
+        switch (vaddr) {
+            default:
+                PLOG_FATAL << "Unrecognized write16 (addr = " << std::hex << vaddr << ", data = " << data << ")";
+
+                exit(0);
+        }
+    }
+}
+
+template<>
+void write(u32 vaddr, const u32 data) {
+    // Mask virtual address
+    vaddr = maskAddress(vaddr);
+
+    const u32 page = vaddr >> PAGE_SHIFT;
+
+    // Look up address in page table, write data directly if it's mapped
+    if (pageTable[page] != NULL) {
+        std::memcpy(&pageTable[page][vaddr & PAGE_MASK], &data, sizeof(u32));
+    } else {
+        switch (vaddr) {
+            default:
+                PLOG_FATAL << "Unrecognized write32 (addr = " << std::hex << vaddr << ", data = " << data << ")";
+
+                exit(0);
         }
     }
 }
